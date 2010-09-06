b556d7c87e5e1ee5703f3bb2523a52e085048664
Yong Bakos <projects@humanoriented.com>
2010-09-05 20:05:50 -0600

    Refactored Pearson. Added function for aggregating similar critics.

--- ci/project02/recommendations.py
+++ ci/project02/recommendations.py
@@ -30,32 +30,45 @@ def euclidean_distance(preferences, person1, person2):
     if item in preferences[person2]:
       similar_items[item] = 1
   if len(similar_items) == 0: return 0
-  sum_of_squares = sum([pow(preferences[person1][item]-preferences[person2][item],2) 
-    for item in preferences[person1] if item in preferences[person2]]) 
+  sum_of_squares = sum([pow(preferences[person1][item]-preferences[person2][item],2)
+    for item in preferences[person1] if item in preferences[person2]])
   print similar_items
   return 1 / (1 + sqrt(sum_of_squares))
 
-# Returns the Pearson correlation coefficient for p1 and p2 
-def pearson_correlation(prefs,p1,p2): 
-  # Get the list of mutually rated items 
-  si={} 
-  for item in prefs[p1]: 
-    if item in prefs[p2]: si[item]=1 
-  # Find the number of elements 
-  n=len(si) 
-  # if they are no ratings in common, return 0 
-  if n==0: return 0 
-  # Add up all the preferences 
-  sum1=sum([prefs[p1][it] for it in si]) 
-  sum2=sum([prefs[p2][it] for it in si]) 
-  # Sum up the squares 
-  sum1Sq=sum([pow(prefs[p1][it],2) for it in si]) 
-  sum2Sq=sum([pow(prefs[p2][it],2) for it in si]) 
-  # Sum up the products 
-  pSum=sum([prefs[p1][it]*prefs[p2][it] for it in si]) 
-  # Calculate Pearson score 
-  num=pSum-(sum1*sum2/n) 
-  den=sqrt((sum1Sq-pow(sum1,2)/n)*(sum2Sq-pow(sum2,2)/n)) 
-  if den==0: return 0 
-  r=num/den 
-  return r
+# Returns the Pearson correlation coefficient for person1 and person2
+def pearson_correlation(preferences, person1, person2):
+  # Create a list of mutually rated item keys
+  similar_items = {}
+  for item in preferences[person1]:
+    if item in preferences[person2]: similar_items[item] = 1
+  # Declare the number of elements
+  num_of_elements = len(similar_items)
+  # If there are no ratings in common, the correlation is 0.
+  if num_of_elements == 0: return 0
+  # Add up all the ratings
+  person1_rating_sum = sum([preferences[person1][movie] for movie in similar_items])
+  person2_rating_sum = sum([preferences[person2][movie] for movie in similar_items])
+  # Sum the squares of each person's ratings
+  person1_scores_squared_sum = sum([pow(preferences[person1][movie], 2) for movie in similar_items])
+  person2_scores_squared_sum = sum([pow(preferences[person2][movie], 2) for movie in similar_items])
+  # Sum the products of person1's and person2's ratings for all movies
+  ratings_products_sum = sum([preferences[person1][movie] * preferences[person2][movie] for movie in similar_items])
+  # Calculate Pearson correlation 'score'
+  # p = covariance(x, y) / (stdev(x) * stdev(y))
+  # pNumerator: covariance(x, y)
+  # pDenominator: (stdev(x) * stdev(y))
+  pNumerator = ratings_products_sum - (person1_rating_sum * person2_rating_sum / num_of_elements)
+  pDenominator = sqrt((person1_scores_squared_sum - pow(person1_rating_sum, 2) / num_of_elements) *
+                      (person2_scores_squared_sum - pow(person2_rating_sum, 2) / num_of_elements))
+  if pDenominator == 0: return 0
+  return (pNumerator / pDenominator)
+
+# Returns the most similar ratings from the preferences dictionary for a given person.
+# number_of_results and similarity_function are optional parameters.
+def similar_critics(preferences, person, number_of_results = 5, similarity_function = pearson_correlation):
+  scores = [(similarity_function(preferences, person, compared_person), compared_person)
+                  for compared_person in preferences if compared_person != person]
+  # Sort the list so the most similar person appears at the top
+  scores.sort()
+  scores.reverse()
+  return scores[0:number_of_results]
